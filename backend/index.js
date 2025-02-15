389ad2e5b30445f6e9fdd5edadc5b8d0587c4e77
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-02-15 17:21:09 +0100

    database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 
+dotenv.config({});
 const app = express();
+
+app.get("/home", (req, res) => {
+  return res.status(200).json({
+    message: "Backend",
+    success: true,
+  });
+});
 
 // middlewarre
 app.use(express.json());
@@ -15,8 +25,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at ${PORT}`);
 });

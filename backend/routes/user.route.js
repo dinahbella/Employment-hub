41fa0e6913b11b41fb67723da0a6eecbaffb73f4
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-02-15 18:10:38 +0100

    userRoute

---
+++ backend/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  register,
+  login,
+  updateProfile,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/profile/update").post(updateProfile);
+
+export default router;

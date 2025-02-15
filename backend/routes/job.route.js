2bd0c8ffe2345ea231a2c6c77b15a2ae4f1709d6
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-02-15 21:28:09 +0100

    jobRoute

---
+++ backend/routes/job.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import {
+  postJob,
+  getAllJobs,
+  getAdminJobs,
+  getJobById,
+} from "../controllers/job.controllers.js";
+
+const router = express.Router();
+router.route("/post").post(isAuthenticated, postJob);
+router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/get/admin").post(isAuthenticated, getAdminJobs);
+router.route("/get/:id").get(isAuthenticated, getJobById);
+
+export default router;
